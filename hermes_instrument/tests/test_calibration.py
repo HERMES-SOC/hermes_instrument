63bd37e73e08ba0ec7a5962f30ce9b4e3c57344e
Steven Christe <steven.christe@nasa.gov>
2022-03-17 14:23:31 -0400

    linting fix

--- hermes_instrument/tests/test_calibration.py
+++ hermes_instrument/tests/test_calibration.py
@@ -2,13 +2,10 @@ import pytest
 from hermes_instrument.calibration.calibration import *
 
 
-def test_calibrate_file_exception():
+def test_calibrate_file():
     with pytest.raises(ValueError) as excinfo:
         calibrate_file("datafile_with_no_calib.cdf")
     assert str(excinfo.value) == "Calibration file for datafile_with_no_calib.cdf not found."
-
-
-def test_get_calibration_file():
     assert get_calibration_file("") is None
 
 
