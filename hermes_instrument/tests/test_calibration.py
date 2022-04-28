c902b48710c223a660addfd9751821316a4afd14
Amy Rager <amy.c.rager@gmail.com>
2022-04-27 23:14:25 -0400

    Update test_calibration.py
    Modifying to comply coding standards and added a header with example call

--- hermes_instrument/tests/test_calibration.py
+++ hermes_instrument/tests/test_calibration.py
@@ -1,19 +1,44 @@
+#!/usr/bin/env python
+"""
+Calibration module unit tests
+
+Parameters
+----------
+None
+
+Returns
+-------
+None
+
+Examples
+--------
+# working directory: /workspaces/hermes_instrument
+
+/usr/bin/python3 -m hermes_instrument.tests.test_calibration
+
+"""
+## IMPORT MODULES ##
 import pytest
-from hermes_instrument.calibration.calibration import *
+from hermes_instrument.calibration import calibration
 
-
+## DEFINE FUNCTIONS ##
 def test_calibrate_file():
     with pytest.raises(ValueError) as excinfo:
-        calibrate_file("datafile_with_no_calib.cdf")
-    assert (
-        str(excinfo.value)
-        == "Calibration file for datafile_with_no_calib.cdf not found."
-    )
+        calibration.calibrate_file("datafile_with_no_calib.cdf")
+    assert str(excinfo.value) == "Calibration file for datafile_with_no_calib.cdf not found."
 
 
 def test_get_calibration_file():
-    assert get_calibration_file("") is None
+    assert calibration.get_calibration_file("") is None
 
 
 def test_read_calibration_file():
-    assert read_calibration_file("calib_file") is None
+    assert calibration.read_calibration_file("calib_file") is None
+
+
+## MAIN PROGRAM ##
+if __name__ == "__main__":
+    print("Starting unit test: new_cal.py")
+    test_calibrate_file()
+    test_get_calibration_file()
+    test_read_calibration_file()
