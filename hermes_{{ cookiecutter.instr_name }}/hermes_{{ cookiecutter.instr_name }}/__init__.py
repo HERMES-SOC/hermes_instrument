fff54637e8cb4164f84b6e402a351c324f68eac4
Steven Christe <ehsteve@users.noreply.github.com>
2023-01-26 18:58:33 -0500

    Packaging updates to modernize and fix version number (#29)

--- hermes_{{ cookiecutter.instr_name }}/hermes_{{ cookiecutter.instr_name }}/__init__.py
+++ hermes_{{ cookiecutter.instr_name }}/hermes_{{ cookiecutter.instr_name }}/__init__.py
@@ -2,9 +2,11 @@
 from hermes_core import log
 
 try:
-    from .version import __version__
+    from ._version import version as __version__
+    from ._version import version_tuple
 except ImportError:
-    __version__ = "unknown"
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
 from hermes_{{ cookiecutter.instr_name }}.io.file_tools import read_file
 
 # from hermes_core.util.config import load_config, print_config
